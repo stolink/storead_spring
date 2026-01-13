c460da2b96ab6817ac3932f88dc07383e1155a5e
minjung0233 <minjung030128@gmail.com>
2026-01-14 03:22:38 +0900

    fix: 댓글 생성 요청 DTO 필드 추가

--- src/main/java/com/stolink/backend/domain/comment/dto/CreateCommentRequest.java
+++ src/main/java/com/stolink/backend/domain/comment/dto/CreateCommentRequest.java
@@ -3,8 +3,10 @@ package com.stolink.backend.domain.comment.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class CreateCommentRequest {
 
